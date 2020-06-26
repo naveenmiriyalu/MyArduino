ba39e2387078c3c7175a432b22755afe5aef005b
Naveen <naveen.miriyalu@gmail.com>
2020-06-26 17:47:36 +0530

    Arduino Fun

---
+++ MyFirstBlinkArduino/MyFirstBlinkArduino.ino
@@ -0,0 +1,11 @@
+int led = 8; // the pin the LED is connected to
+void setup() {
+  pinMode(led, OUTPUT); // Declare the LED as an output
+}
+
+void loop() {
+  digitalWrite(led, HIGH); // Turn the LED on
+  delay(3000);
+  digitalWrite(led, LOW); // Turn the LED on
+  delay(3000);
+}
